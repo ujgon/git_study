15d58ee6f7464b09df67ec153c9aca7be54dee18
erinleeme <gusdk1793@gmail.com>
2023-12-12 00:01:43 +0900

    logout 기능 구현

--- project/src/main/java/com/ll/medium/domain/Member/controller/MemberController.java
+++ project/src/main/java/com/ll/medium/domain/Member/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.ll.medium.domain.Member.controller;
 
 import com.ll.medium.domain.Member.dto.JoinRequestDto;
+import com.ll.medium.domain.Member.entity.Member;
 import com.ll.medium.domain.Member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -27,12 +30,29 @@ public class MemberController {
     }
 
     @PostMapping("/member/login")
-    public String login(@RequestBody JoinRequestDto joinRequestDto ) {
-        String result = memberService.login(joinRequestDto);
-        if (result.equals("true")) {
-            return "맞습니다.";
-        } else {
+    public String login(HttpServletRequest request, @RequestBody JoinRequestDto joinRequestDto ) {
+        Member result = memberService.login(joinRequestDto);
+        //서비스에서 로그인이 성공하면 member 보내주고 실패하면 null 보내준다.
+        if(result == null) {
             return "틀립니다.";
         }
+        //로그인 성공하면 Session에 사용자 정보를 넣어야함
+        //1. request로부터 가져온다.
+        HttpSession session = request.getSession();
+        //2. session에 member 정보 저장
+        session.setAttribute("Member", result);
+        return "맞습니다.";
+    }
+
+    @PostMapping("/member/logout")
+    public String logout(HttpServletRequest request) {
+        //1. 먼저 현재 session이 있는지 없는지 확인
+        HttpSession session = request.getSession(false);
+        //2. session이 있다면 session 삭제
+        if(session != null){
+            session.invalidate();
+            return "로그아웃 했습니다.";
+        }
+        return "로그아웃 실패";
     }
 }
