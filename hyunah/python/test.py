88402ec10dd900cda352f1a87c45a2fb4db9e178
erinlee <gusdk1793@gmail.com>
2024-03-11 16:51:31 +0900

    hyunah/입출력문제

--- hyunah/python/test.py
+++ hyunah/python/test.py
@@ -1,17 +1,3 @@
-n = int(input())
-
-for i in range(n):
-    a, b = map(int, input().split())
-    print("Case #%d: %d + %d = %d" % (i+1, a, b, a+b))
-
-
-
-
-
-# import sys
-
-# T = int(input())
-
-# for i in range(T):
-#     a,b = map(int,sys.stdin.readline().split())
-#     print(f"Case #{i+1}: {a} + {b} = {a+b}")
+n = 5
+for i in range(n*2-1, 0, -1):
+    print("%s%s" % (" "*abs(i-n), "*"*(n-abs(i-n))))
