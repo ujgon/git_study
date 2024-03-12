ef2acfcf4366ef9f048bf5be7f5d01ee79b0c6a1
erinlee <gusdk1793@gmail.com>
2024-03-12 16:55:16 +0900

    hyunah/입출력완료

--- hyunah/python/test.py
+++ hyunah/python/test.py
@@ -1,3 +1,6 @@
-n = 5
-for i in range(n*2-1, 0, -1):
-    print("%s%s" % (" "*abs(i-n), "*"*(n-abs(i-n))))
+n=int(input())
+
+for i in range(n-1):
+    print(' '*(n-i-1)+'*'+' '*(2*i-1)+'*'*(i and 1))
+    
+print('*'*(2*n-1))
